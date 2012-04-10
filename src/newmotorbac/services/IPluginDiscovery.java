a4118830adbc4da4260ec961e772ea2ae770f9fd
Firas Al Khalil <firasalkhalil@gmail.com>
2012-04-10 08:45:44 -1000

    Initializing repository with service discovery support

---
+++ src/newmotorbac/services/IPluginDiscovery.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newmotorbac.services;
+
+/**
+ *
+ * @author fkhalil
+ */
+public interface IPluginDiscovery {
+    public boolean contributesToPolicy();
+}
